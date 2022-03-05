10fa33f20cc37fbf8f766aecf797dee6504ac72e
Elias Sundqvist <elias@sundqvist.com>
2022-03-05 18:28:28 +0100

    Set _hypothesisAppsOrigin to '*'

--- rollup-boot.config.mjs
+++ rollup-boot.config.mjs
@@ -17,19 +17,13 @@ const { version } = JSON.parse(readFileSync('./package.json').toString());
 // URL template which is expanded by the boot script. See `src/boot/url-template.js`.
 const localhost = '{current_scheme}://{current_host}';
 
-const notebookAppUrl = process.env.NOTEBOOK_APP_URL
-  ? `${process.env.NOTEBOOK_APP_URL}`
-  : `${localhost}:5000/notebook`;
+const notebookAppUrl = `https://hypothes.is/notebook`;
 
-const sidebarAppUrl = process.env.SIDEBAR_APP_URL
-  ? `${process.env.SIDEBAR_APP_URL}`
-  : `${localhost}:5000/app.html`;
+const sidebarAppUrl = `https://hypothes.is/app.html`;
 
 // nb. Replace `isProd` with `false` here to test a production build of the client
 // served locally.
-const assetRoot = isProd
-  ? `https://cdn.hypothes.is/hypothesis/${version}/`
-  : `${localhost}:3001/hypothesis/${version}/`;
+const assetRoot = `https://cdn.hypothes.is/hypothesis/`;
 
 export default {
   input: 'src/boot/index.js',
